22e18cb64c4455a25dfd759550bcf8bf4ce31439
Zloied <zloied11@gmail.com>
2019-02-04 18:38:03 +0100

    Test reconfiguration

--- src/test/java/com/vais/RentalApplicationTests.java
+++ src/test/java/com/vais/RentalApplicationTests.java
@@ -1,17 +1,23 @@
 package com.vais;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RentalApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(applicationContext);
 	}
 
 }
